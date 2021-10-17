76f0d4f007453d7d2695f3e601746f38801bcbb9
chrnnsa <chairun089@gmail.com>
2021-10-17 09:34:06 +0700

    row and column

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BelajarRowColumn());
+
+class BelajarRowColumn extends StatelessWidget {
+  const BelajarRowColumn({Key? key}) : super(key: key);
+
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Belajar Row Column',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Aplikasiku(),
+    ); 
+  }
+}
+
+class Aplikasiku extends StatelessWidget {
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text('Row & Column'),
+      ),
+      body: Container( 
+        child: Row(
+       mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Icon(Icons.home),Text('Home')]
+              )),
+
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Icon(Icons.search),Text('Search')]
+              )),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Icon(Icons.account_circle),Text('Profile')]
+              )),
+        ],
+        )
+      ),
+    );
+  }
+}
